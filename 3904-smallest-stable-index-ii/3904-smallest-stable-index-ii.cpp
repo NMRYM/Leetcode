69479093cd1367093cfd5e47684a5f7909fe9f52
Nasmiya Mariyam <nasmiya2006@gmail.com>
2026-09-05 11:52:42 +0530

    Time: 11 ms (57.63%), Space: 210.9 MB (15.41%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -5,20 +5,17 @@ public:
         int maxx=0,minn = INT_MAX;
 
         for(int i =0 ; i < nums.size() ;i++){
-            if(nums[i] > maxx){
-                maxx= nums[i];
-            }
-
-            if(nums[nums.size()-i-1] < minn){
-                minn= nums[nums.size() - i -1];
-            }
+                maxx= max(maxx,nums[i]);
+                minn= min(minn,nums[nums.size() - i -1]);
+            
 
             maxi[i] = maxx;
             mini[nums.size() - i -1]=minn;
         }
 
         for(int i = 0 ; i < nums.size() ; i++ ){
-            if(maxi[i] - mini[i] <=k){
+            int s=maxi[i] - mini[i];
+            if(s <=k){
                 return i;
             }
         }
